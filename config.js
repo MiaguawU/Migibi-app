19f70ecd2c09d97bb2e3d9c690c0c8318824738a
MiaguawU <isis.miaguaw@gamil.com>
2025-05-01 15:35:16 -0600

    config.js a ip

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const PUERTO = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000';
+const PUERTO = process.env.REACT_APP_SERVER_URL || 'http://192.168.0.242:5000';
 console.log("El valor de PUERTO es:", PUERTO); // Para depuración
 export default PUERTO;
